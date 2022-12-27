17367a284e5892128df95f2006774c3cf5c65f84
kanakkhandelwal25~ <kkhandelwal1590@gmail.com>
2022-12-27 22:42:37 +0530

    commit #6

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,11 +13,36 @@ const Expenses = (props) => {
 
     const [filteredYear,setFilteredYear] = useState('2020');
 
+    const filteredExpenses = props.obj.filter(expenses => {
+        return expenses.date.getFullYear().toString() === filteredYear;
+    })
+
+    let expensesContent = <p>No expenses found</p>;
+
+    if(filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem
+                key = {expense.id}
+                title = {expense.title}
+                amount = {expense.amount}
+                date = {expense.date} />
+        ))
+    }
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                {props.obj.map((expense) => (<ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>))}
+                {/* {props.obj.map((expense) => (<ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>))} */}
+                {/* {filteredExpenses.length === 0 ? (<p>No expenses found</p>) : (     //conditional expression
+                    filteredExpenses.map((expense) => (
+                    <ExpenseItem 
+                        key={expense.id} 
+                        title={expense.title} 
+                        amount={expense.amount} 
+                        date={expense.date}/>
+                    ))
+                )} */}
+                {expensesContent}
                 {/* <ExpenseItem title = {props.obj[0].title} amount = {props.obj[0].amount} date = {props.obj[0].date}></ExpenseItem>
                 <ExpenseItem title = {props.obj[1].title} amount = {props.obj[1].amount} date = {props.obj[1].date}></ExpenseItem>
                 <ExpenseItem title = {props.obj[2].title} amount = {props.obj[2].amount} date = {props.obj[2].date}></ExpenseItem>
